395d54fbae696caed438cc325c56aa2a5827a4f6
danilagundyrev <57663598+danilagundyrev@users.noreply.github.com>
2022-09-26 02:17:04 +0200

    Create Keywords.cs

---
+++ UnityVoiceAssistant/Keywords.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+public static class Keywords
+{
+    public static Dictionary<int, string[]> KeywordsDictionary = new Dictionary<int, string[]>()
+    {
+        {0, new string[] { "some", "keywords", "to", "imitate", "this", "feature" } },
+        {1, new string[] { "some", "keywords", "to", "imitate", "this", "feature" } },
+        {2, new string[] { "some", "keywords", "to", "imitate", "this", "feature" } }       
+    };
+}
